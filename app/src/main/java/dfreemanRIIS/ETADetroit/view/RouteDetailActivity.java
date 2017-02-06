a82fa035b1fdac07319919fa0ddf449175e2723b
dfreemanRIIS <dfreeman@riis.com>
2017-02-06 14:06:57 -0500

    Revert "This is more like the real MVP layout i think"
    This reverts commit ad2f2766cd8172156da628a813b66d489af38700.

--- app/src/main/java/dfreemanRIIS/ETADetroit/view/RouteDetailActivity.java
+++
@@ -1,8 +0,0 @@
-package dfreemanRIIS.ETADetroit.view;
-
-/**
- * Created by dfreeman on 2/6/17.
- */
-
-public class RouteDetailActivity {
-}
--- app/src/main/java/dfreemanRIIS/ETADetroit/view/RouteDetailView.java
+++ app/src/main/java/dfreemanRIIS/ETADetroit/view/RouteDetailView.java
@@ -1,4 +1,4 @@
-package dfreemanRIIS.ETADetroit.acitivity;
+package dfreemanRIIS.ETADetroit.view;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -11,7 +11,7 @@ import android.database.Cursor;
 import dfreemanRIIS.ETADetroit.R;
 import dfreemanRIIS.ETADetroit.presenter.RouteDetailPresenter;
 
-public class RouteDetailActivity extends Activity {
+public class RouteDetailView extends Activity {
 
     public static final String EXTRA_ROUTE_NAME = "route_name";
 
